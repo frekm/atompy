a39ec68ac3a701798c095664d6a4a1405f2e86f5
Max Kircher <max@cronologic.de>
2024-03-19 11:29:14 +0100

    Changed html theme to Furo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,12 +36,12 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'pydata_sphinx_theme'
+html_theme = 'furo'
 html_static_path = ['_static']
 
-html_theme_options = {
-    "show_toc_level": 4,
-    "collapse_navigation": False,
-    "navigation_with_keys": False,
-    "show_nav_level": 4,
-}
+# html_theme_options = {
+#     "show_toc_level": 4,
+#     "collapse_navigation": False,
+#     "navigation_with_keys": False,
+#     "show_nav_level": 4,
+# }

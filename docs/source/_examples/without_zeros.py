80b347bb6a8c5ed1d8c615f54744d8f5f85fc0c6
Max Kircher <max@cronologic.de>
2024-04-11 11:25:52 +0200

    Fix #15
    Add colorbars to Hist2d.without_zeros example.

--- docs/source/_examples/without_zeros.py
+++ docs/source/_examples/without_zeros.py
@@ -8,9 +8,16 @@ plt.rcParams["image.aspect"] = "auto"
 gen = np.random.default_rng(42)
 hist = ap.Hist2d(*np.histogram2d(*gen.normal(size=(2, 100))))
 
-fig, ax = ap.subplots(ncols=2, ratio=1)
+fig, ax_ = ap.subplots(ncols=3, ratio=1)
+ax = ap.abcify_axes(ax_)
 
-ax[0][0].imshow(**hist.for_imshow())                # left panel
-ax[0][1].imshow(**hist.without_zeros.for_imshow())  # right panel
+im = [] # empty list to store colorbar images in
+im.append(ax["a"].imshow(**hist.for_imshow()))
+im.append(ax["b"].imshow(**hist.without_zeros.for_imshow(), vmin=0))
+im.append(ax["c"].imshow(**hist.without_zeros.for_imshow()))
 
-ap.make_margins_tight(ax, pad=5)
+colorbars = ap.add_colorbar(ax_, im, where="top")
+ap.make_margins_tight(ax_, pad=5, colorbars=colorbars)
+ap.add_abc(ax_, prefix="(", suffix=")")
+
+

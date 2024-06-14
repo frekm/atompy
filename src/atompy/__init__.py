28581ec0aaab14685393407a1db526acedccfe05
Max Kircher <max@cronologic.de>
2024-06-14 11:18:51 +0200

    Update __init__
    instead of wildcard-import, import only relevant functions

--- src/atompy/__init__.py
+++ src/atompy/__init__.py
@@ -1,8 +1,59 @@
-from ._io import *
-from ._io2 import *
-from ._miscellaneous import *
-from ._histogram import *
-from ._vector import *
-from ._plotting import *
+from ._io import (
+    save_1d_as_txt,
+    save_2d_as_txt,
+    load_1d_from_txt,
+    load_2d_from_txt,
+    load_1d_from_root,
+    load_2d_from_root,
+)
+
+from ._miscellaneous import (
+    NonconstantBinsizeError,
+    ImshowData,
+    PcolormeshData,
+    get_all_dividers,
+    crop,
+    convert_cosine_to_angles,
+    integral_sum,
+    integral_polyfit,
+    sample_distribution,
+)
+
+from ._histogram import Hist1d, Hist2d
+
+from ._vector import Vector
+
+from ._plotting import (
+    AliasError,
+    FigureWidthTooLargeError,
+    Edges,
+    PTS_PER_INCH,
+    MM_PER_INCH,
+    colors,
+    cm_lmf2root,
+    cm_lmf2root_from_white,
+    textwithbox,
+    dotted,
+    dash_dotted,
+    dashed,
+    dashed,
+    add_colorbar,
+    add_abc,
+    update_colorbars,
+    get_renderer,
+    set_axes_size,
+    get_sorted_axes_grid,
+    get_column_pads_inches,
+    set_min_column_pads,
+    get_row_pads_inches,
+    set_min_row_pads,
+    get_figure_margins_inches,
+    get_axes_position_inch,
+    get_axes_tightbbox_inch,
+    make_me_nice,
+    align_axes_vertically,
+    align_axes_horizontally,
+    get_axes_margins_inches,
+)
 
 from ._version import __version__

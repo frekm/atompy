b3804d2278c4c893dcef08ba0e12f5bfa9e5d1db
Max Kircher <max@cronologic.de>
2024-07-01 17:27:43 +0200

    Restructure example hierarchy

--- docs/source/_examples/single_plot_2D_pcolormesh.py
+++ docs/source/_examples/single_plot_2D_pcolormesh.py
@@ -4,10 +4,13 @@ Plot a single 2D-Histogram from a ROOT file.
 import atompy as ap
 import matplotlib.pyplot as plt
 
+# configure using rcParams
+plt.rcParams["figure.figsize"] = 3., 3. # the height will be adjusted by make_me_nice()
+
 # load 2D histogram from root file to plot it with imshow
-image, extents = ap.load_2d_from_root(
+x, y, z = ap.load_2d_from_root(
     "example.root", "He_Compton/electrons/momenta/px_vs_py",
-    output_format="imshow")
+    output_format="pcolormesh")
 
 # format figure
 plt.rcParams["image.cmap"] = "lmf2root"
@@ -19,7 +22,7 @@ ax = plt.subplot()
 ax.set_box_aspect(1.0)
 
 # plot 
-cmap_image = ax.imshow(image, extent=extents)
+cmap_image = ax.pcolormesh(x, y, z, rasterized=True)
 
 # add a colorbar
 cb = ap.add_colorbar(cmap_image, ax)
---
+++ docs/source/examples/plotting/single_2d_imshow.rst
@@ -0,0 +1,5 @@
+2D single plot using :func:`imshow <matplotlib.pyplot.imshow>`
+--------------------------------------------------------------
+
+.. plot:: _examples/single_plot_2d_imshow.py
+    :include-source:

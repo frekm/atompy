8f8fb49c93101012324b259673a7a70d15a6d69e
Max Kircher <max@cronologic.de>
2025-02-28 12:13:32 +0100

    reformatting

--- docs/source/_examples/histogram/create_hist1ds.py
+++ docs/source/_examples/histogram/create_hist1ds.py
@@ -11,26 +11,26 @@ data = 2 * np.random.default_rng(42).integers(-10, 11, 100)
 nbins = 10
 hist1 = ap.Hist1d(*np.histogram(data, bins=nbins))
 
-axs[0,0].set_title("fixed number of bins")
-axs[0,0].bar(*hist1.for_plot, width=hist1.binwidths)
-axs[1,0].step(*hist1.for_step)
+axs[0, 0].set_title("fixed number of bins")
+axs[0, 0].bar(*hist1.for_plot, width=hist1.binwidths)
+axs[1, 0].step(*hist1.for_step)
 
 
 # histogram with fixed width of bins
 binwidth = 1.0
-start = data.min() - binwidth/2.0
+start = data.min() - binwidth / 2.0
 stop = data.max() + binwidth
 bin_edges = np.arange(start, stop, binwidth)
 hist2 = ap.Hist1d(*np.histogram(data, bins=bin_edges))
 
-axs[0,1].set_title(f"fixed bin width")
-axs[0,1].bar(*hist2.for_plot, width=hist2.binwidths)
-axs[1,1].step(*hist2.for_step)
+axs[0, 1].set_title(f"fixed bin width")
+axs[0, 1].bar(*hist2.for_plot, width=hist2.binwidths)
+axs[1, 1].step(*hist2.for_step)
 
 # histogram counting the occurance of values
-axs[0,2].set_title(f"numpy.unique")
-axs[0,2].bar(*np.unique(data, return_counts=True), width=1)
-axs[1,2].step(*np.unique(data, return_counts=True), where="mid")
+axs[0, 2].set_title(f"numpy.unique")
+axs[0, 2].bar(*np.unique(data, return_counts=True), width=1)
+axs[1, 2].step(*np.unique(data, return_counts=True), where="mid")
 
 
 for ax in axs.flat:

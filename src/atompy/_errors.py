aa316a95d131c1151044fa6bf06dd808b92a9445
Max Kircher <max@cronologic.de>
2026-01-23 13:02:13 +0100

    remvoe AliasError

--- src/atompy/_errors.py
+++ src/atompy/_errors.py
@@ -4,15 +4,3 @@ class UnmatchingEdgesError(Exception):
     def __init__(self, which):
         self.message = f"histogram {which}edges don't match"
         super().__init__(self.message)
-
-
-class AliasError(Exception):
-    def __init__(self, keyword_arg: str, alias: str):
-        self.keyword_arg = keyword_arg
-        self.alias = alias
-
-    def __str__(self):
-        return (
-            f"Both '{self.keyword_arg}' and '{self.alias}' have been "
-            "provided, but they are aliases"
-        )

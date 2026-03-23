cc4779fb15cad7cf74b84de5fc1abca242b6ddaa
Max Kircher <max@cronologic.de>
2026-03-23 09:40:15 +0100

    work on data_xyz

--- src/atompy/_data_xyz.py
+++ src/atompy/_data_xyz.py
@@ -1,8 +1,11 @@
+from typing import Literal
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.axes import Axes
 import numpy as np
-from numpy.typing import ArrayLike
+from numpy.typing import ArrayLike, NDArray
+
+import atompy._utils as utils
 
 
 class DataXYZ:
@@ -28,6 +31,11 @@ class DataXYZ:
         if self._y.size != self._z.shape[1]:
             raise ValueError("mismatch of y and z data")
 
+        self._x = np.asarray(x)
+        self._y = np.asarray(y)
+        self._z = np.asarray(z)
+        self._data = np.array((x, y, z))
+
         self._pcolormesh_kwargs = pcolormesh_kwargs
         self._pcolormesh_kwargs.setdefault("rasterized", True)
         self._pcolormesh_kwargs.setdefault("shading", "nearest")
@@ -36,6 +44,65 @@ class DataXYZ:
         self._ylabel = ylabel
         self._zlabel = zlabel
 
-    def plot(self) -> tuple[Figure, Axes]:
-        plt.pcolormesh(self._x, self._y, self._z.T, **self._pcolormesh_kwargs)
+    @property
+    def x(self) -> NDArray[np.number]:
+        return self._data[0]
+
+    @property
+    def y(self) -> NDArray[np.number]:
+        return self._data[1]
+
+    @property
+    def z(self) -> NDArray[np.number]:
+        return self._data[2]
+
+    @x.setter
+    def x(self, vals: ArrayLike) -> None:
+        self._data[0] = vals
+
+    @y.setter
+    def y(self, vals: ArrayLike) -> None:
+        self._data[1] = vals
+
+    @z.setter
+    def z(self, vals: ArrayLike) -> None:
+        self._data[2] = vals
+
+    @property
+    def xyz(self) -> NDArray[np.number]:
+        return self._data
+
+    def x_values(self) -> NDArray[np.number]:
+        return np.unique(self.x)
+
+    def y_values(self) -> NDArray[np.number]:
+        return np.unique(self.y)
+
+    def size_x(self) -> int:
+        return len(self.x_values())
+
+    def size_y(self) -> int:
+        return len(self.y_values())
+
+    def iteration_order(self) -> Literal["x_first", "y_first"]:
+        xs = self.x_values()
+        if xs[1] != self.x[1]:
+            return "x_first"
+        ys = self.y_values()
+        if ys[1] != self.y[1]:
+            return "y_first"
+        raise ValueError("cannot determine iteration order of x and y data")
+
+    def z_grid(self) -> NDArray[np.number]:
+        z_ = self.z
+        if self.iteration_order == "x_first":
+            z_ = z_.reshape(self.size_y(), self.size_x()).T
+        else:
+            z_ = z_.reshape(self.size_x(), self.size_y())
+        return z_
+
+    def plot(self, ax: Axes | None = None) -> tuple[Figure, Axes]:
+        ax = ax or plt.gca()
+        ax.pcolormesh(self._x, self._y, self._z.T, **self._pcolormesh_kwargs)
+        # ax.imshow(self._z)
         return plt.gcf(), plt.gca()
